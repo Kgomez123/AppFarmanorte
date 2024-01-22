2c48a685abf34374f31160b8041351c9749d7eb0
Kgomez123 <keng7015@gmail.com>
2024-01-22 17:16:15 -0500

    Ajuste modulo detalles

--- composeApp/src/commonMain/kotlin/DetallesPedido.kt
+++ composeApp/src/commonMain/kotlin/DetallesPedido.kt
@@ -1,13 +1,20 @@
+
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
@@ -22,13 +29,19 @@ class DetallesPedido(pedidos: Pedidos): Screen {
     @Composable
     override fun Content() {
         val navigator: Navigator = LocalNavigator.currentOrThrow
-        Column(Modifier.fillMaxWidth().verticalScroll(rememberScrollState()), horizontalAlignment = Alignment.CenterHorizontally) {
+        Column(Modifier.fillMaxWidth().fillMaxHeight().verticalScroll(rememberScrollState()), horizontalAlignment = Alignment.CenterHorizontally) {
             Text("Pedido N° ${pedidos.codpedido}")
             Spacer(modifier = Modifier.height(20.dp))
             Text("Dirección: ${pedidos.direccion_pedido}")
             Text("Pago:  ${pedidos.tipo_pago}")
             Text("Estado:  ${pedidos.estado}")
             Text("Observaciones:  ${pedidos.observacion_adicional}")
+            Spacer(modifier = Modifier.weight(1f))
+            Button(onClick = {
+
+            }, shape = RoundedCornerShape(20.dp), contentPadding = PaddingValues(horizontal = 25.dp, vertical = 10.dp), colors = ButtonDefaults.buttonColors(backgroundColor = Color(0,51,153), contentColor = Color(0,175,239))) {
+                Text(text = "Seguimiento")
+            }
         }
     }
 }
